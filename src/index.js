ffa5d2dd3fca38d9b293797c44fadc23b965f3de
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-02-24 09:44:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,27 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	await fs.writeFile('File.txt',' World');
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	const fileContent = await fs.readFile('File.txt')
+	return fileContent.toString();
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	await fs.appendFile('File.txt',' World');
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	return await fs.unlink('File.txt')
 }
 
 
